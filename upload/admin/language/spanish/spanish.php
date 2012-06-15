6afa3d1ad5a42cbeefa291d05a75d221b36ee142
rowend <rowend07@gmail.com>
2012-06-15 18:52:00 -0500

    error estep

--- upload/admin/language/spanish/spanish.php
+++ upload/admin/language/spanish/spanish.php
@@ -103,12 +103,12 @@ $_['tab_voucher']             = 'Vales';
 $_['tab_voucher_history']     = 'Hitorial del Vale';
 
 // Error
-$_['error_upload_1']          = 'Warning: The uploaded file exceeds the upload_max_filesize directive in php.ini!';
-$_['error_upload_2']          = 'Warning: The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form!';
-$_['error_upload_3']          = 'Warning: The uploaded file was only partially uploaded!';
-$_['error_upload_4']          = 'Warning: No file was uploaded!';
-$_['error_upload_6']          = 'Warning: Missing a temporary folder!';
-$_['error_upload_7']          = 'Warning: Failed to write file to disk!';
-$_['error_upload_8']          = 'Warning: File upload stopped by extension!';
-$_['error_upload_999']        = 'Warning: No error code available!';
+$_['error_upload_1']          = 'Cuidado: El archivo subido excede la directiva upload_max_filesize de php.ini!';
+$_['error_upload_2']          = 'Cuidado: El archivo subido excede la directavia MAX_FILE_SIZE especificada en el formulario HTML!';
+$_['error_upload_3']          = 'Cuidado: El archivo solo fue parcialmente subido!';
+$_['error_upload_4']          = 'Cuidado: No se subió ningún archivo!';
+$_['error_upload_6']          = 'Cuidado: Falta la carpeta temporal!';
+$_['error_upload_7']          = 'Cuidado: Error al escribir en disco!';
+$_['error_upload_8']          = 'Cuidado: Subida de archivo interrumpida por la extensión!';
+$_['error_upload_999']        = 'Cuidado: Código de error no disponible!';
 ?>
