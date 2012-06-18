b9087c33062a231a03542d97511487822e854230
rowend <rowend07@gmail.com>
2012-06-18 14:16:04 -0500

    admin-design-banner file

--- upload/admin/language/spanish/design/banner.php
+++ upload/admin/language/spanish/design/banner.php
@@ -3,30 +3,30 @@
 $_['heading_title']      = 'Banners';
 
 // Text
-$_['text_success']       = 'Success: You have modified banners!';
-$_['text_default']       = 'Default';
-$_['text_image_manager'] = 'Image Manager';
-$_['text_browse']        = 'Browse Files';
-$_['text_clear']         = 'Clear Image';
+$_['text_success']       = 'Éxito: has modificado los banners!';
+$_['text_default']       = 'Por defecto';
+$_['text_image_manager'] = 'Gestor de imágenes';
+$_['text_browse']        = 'Examinar Archivos';
+$_['text_clear']         = 'Quitar Imágen';
 
 // Column
-$_['column_name']        = 'Banner Name';
-$_['column_status']      = 'Status';
-$_['column_action']      = 'Action';
+$_['column_name']        = 'Nombre del banner';
+$_['column_status']      = 'Estatus';
+$_['column_action']      = 'Acción';
 
 // Entry
-$_['entry_name']         = 'Banner Name:';
-$_['entry_title']        = 'Title:';
-$_['entry_link']         = 'Link:';
-$_['entry_image']        = 'Image:';
-$_['entry_status']       = 'Status:';
+$_['entry_name']         = 'Nombre del banner:';
+$_['entry_title']        = 'Título:';
+$_['entry_link']         = 'Enlace:';
+$_['entry_image']        = 'Imagen:';
+$_['entry_status']       = 'Estatus:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify banners!';
-$_['error_name']         = 'Banner Name must be between 3 and 64 characters!';
-$_['error_title']        = 'Banner Title must be between 2 and 64 characters!';
-$_['error_default']      = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
-$_['error_product']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s products!';
-$_['error_category']     = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
-$_['error_information']  = 'Warning: This layout cannot be deleted as it is currently assigned to %s information pages!';
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar banners!';
+$_['error_name']         = 'El nombre del banner debe tener entre 3 y 64 carácteres!';
+$_['error_title']        = 'El título del banner debe tener entre 2 y 64 carácteres!';
+$_['error_default']      = 'Cuidado: Este diseño no se puede borrar ya que esta actualmentee asignado como el diseño por defecto de la tienda!';
+$_['error_product']      = 'Cuidado: Este diseño no se puede borrar ya que está actualmente asignado a %s productos!';
+$_['error_category']     = 'Cuidado: Este diseño no se puede borrar ya que está actualmente asignado a %s categorías!';
+$_['error_information']  = 'Cuidado: Este diseño no se puede borrar ya que está actualmente asignado a %s páginas de información!';
 ?>
