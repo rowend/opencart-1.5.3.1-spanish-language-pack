e79d74786cdea5d83a95a68c44d33706f1ab47b4
rowend <rowend07@gmail.com>
2012-06-19 11:35:09 -0500

    admin-localisation-geozone file

--- upload/admin/language/spanish/localisation/geo_zone.php
+++ upload/admin/language/spanish/localisation/geo_zone.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']      = 'Geo Zones';
+$_['heading_title']      = 'Zonas Geográficas';
 
 // Text
-$_['text_success']       = 'Success: You have modified geo zones!';
+$_['text_success']       = 'Éxito: has modificado las zonas geográficas!';
 
 // Column
-$_['column_name']        = 'Geo Zone Name';
-$_['column_description'] = 'Description';
-$_['column_action']      = 'Action';
+$_['column_name']        = 'Nombre de la Zona Goegráfica';
+$_['column_description'] = 'Descripción';
+$_['column_action']      = 'Acción';
 
 // Entry
-$_['entry_name']         = 'Geo Zone Name:';
-$_['entry_description']  = 'Description:';
-$_['entry_country']      = 'Country:';
-$_['entry_zone']         = 'Zone:';
+$_['entry_name']         = 'Nombre de la Zona Goegráfica:';
+$_['entry_description']  = 'DDescripción:';
+$_['entry_country']      = 'País:';
+$_['entry_zone']         = 'Zona:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify geo zones!';
-$_['error_name']         = 'Geo Zone Name must be between 3 and 32 characters!';
-$_['error_description']  = 'Description Name must be between 3 and 255 characters!';
-$_['error_tax_rate']     = 'Warning: This geo zone cannot be deleted as it is currently assigned to one or more tax rates!';
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar las zonas geográficas!';
+$_['error_name']         = 'El nombre geográfico de  debe tener entre 3 y 32 carácteres!';
+$_['error_description']  = 'La descripción debe tener entre 3 y 255 carácteres!';
+$_['error_tax_rate']     = 'Cuidado: Esta zona geográfica no se puede borrar ya que está actualmente asignado a una o más impuestos!';
 ?>
