8e71ba6f17953f370ae285f46bd747b70102983d
rowend <rowend07@gmail.com>
2012-06-19 13:40:10 -0500

    admin-localisation-return-status

--- upload/admin/language/spanish/localisation/return_status.php
+++ upload/admin/language/spanish/localisation/return_status.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']    = 'Return Status';
+$_['heading_title']    = 'Estado de devolución';
 
 // Text
-$_['text_success']     = 'Success: You have modified return statuses!';
+$_['text_success']     = 'Éxito: has modificado los estados de devolución!!';
 
 // Column
-$_['column_name']      = 'Return Status Name';
-$_['column_action']    = 'Action';
+$_['column_name']      = 'Nombre del estado de devolución';
+$_['column_action']    = 'Acción';
 
 // Entry
 $_['entry_name']       = 'Return Status Name:';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify return statues!';
-$_['error_name']       = 'Return Status Name must be between 3 and 32 characters!';
-$_['error_default']    = 'Warning: This return status cannot be deleted as it is currently assigned as the default return status!';
-$_['error_return']     = 'Warning: This return status cannot be deleted as it is currently assigned to %s returns!';
+$_['error_name']       = 'El nombre del estado de devolución debe tener entre 3 y 32 carácteres!!';
+$_['error_default']    = 'Cuidado: Este estado de devolución no se puede borrar ya que esta actualmentee asignado como el estado de devolución por defecto!';
+$_['error_return']     = 'Cuidado: Este estado de devolución no se puede borrar ya que está actualmente asignado a %s devoluciones!';
 ?>
