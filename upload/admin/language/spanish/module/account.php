e8236ecb386bbdd653909aa99ffe6f5884578b16
rowend <rowend07@gmail.com>
2012-06-22 10:19:13 -0500

    admin-module-account

--- upload/admin/language/spanish/module/account.php
+++ upload/admin/language/spanish/module/account.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']       = 'Account';
+$_['heading_title']       = 'Cuenta';
 
 // Text
-$_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module account!';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
+$_['text_module']         = 'Módulos';
+$_['text_success']        = 'Éxito: has modificado el módulo de cuenta!';
+$_['text_content_top']    = 'Contenido superior';
+$_['text_content_bottom'] = 'Contenido inferior';
+$_['text_column_left']    = 'Columna izquierda';
+$_['text_column_right']   = 'Columna derecha';
 
 // Entry
-$_['entry_layout']        = 'Layout:';
-$_['entry_position']      = 'Position:';
-$_['entry_status']        = 'Status:';
-$_['entry_sort_order']    = 'Sort Order:';
+$_['entry_layout']        = 'Diseño:';
+$_['entry_position']      = 'Posición:';
+$_['entry_status']        = 'Estado:';
+$_['entry_sort_order']    = 'Orden de aparición:';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module account!';
+$_['error_permission']    = 'Cuidado: No tienes permisos para modificar el módulo de cuenta!';
 ?>
