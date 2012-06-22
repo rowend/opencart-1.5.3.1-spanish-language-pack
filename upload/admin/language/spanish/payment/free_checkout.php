374d5bc15e4b34d6157ba729c1e611a1506d4e59
rowend <rowend07@gmail.com>
2012-06-22 17:08:10 -0500

    admin-payment-free-checkout

--- upload/admin/language/spanish/payment/free_checkout.php
+++ upload/admin/language/spanish/payment/free_checkout.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']      = 'Free Checkout';
+$_['heading_title']      = 'Compra gratis';
 
 // Text
-$_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified Free Checkout payment module!';
+$_['text_payment']       = 'Pago';
+$_['text_success']       = 'Éxito: has modificado el módulo de pago compra gratis!';
 
 // Entry
-$_['entry_order_status'] = 'Order Status:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_order_status'] = 'Estado del pedido:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de aparición:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Free Checkout!';
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar el pago compra gratis!';
 ?>
