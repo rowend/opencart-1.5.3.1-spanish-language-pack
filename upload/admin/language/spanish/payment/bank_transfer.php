653184c072bf67f2ae5a228aa83700529a468ede
rowend <rowend07@gmail.com>
2012-06-22 16:54:07 -0500

    admin-payment-bank-transfer

--- upload/admin/language/spanish/payment/bank_transfer.php
+++ upload/admin/language/spanish/payment/bank_transfer.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']      = 'Bank Transfer';
+$_['heading_title']      = 'Transferencia bancaria';
 
-// Text 
-$_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified bank transfer details!';
+// Text
+$_['text_payment']       = 'Pago';
+$_['text_success']       = 'Éxito: has modificado los detalles de transferencia bancaria!';
 
 // Entry
-$_['entry_bank']         = 'Bank Transfer Instructions:';
-$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
-$_['entry_order_status'] = 'Order Status:';
-$_['entry_geo_zone']     = 'Geo Zone:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_bank']         = 'Instrucciones para la transferencia bancaria:';
+$_['entry_total']        = 'Total:<br /><span class="help">El total de la orden de pago debe llegar antes de que este método de pago sea activado.</span>';
+$_['entry_order_status'] = 'Estado del pedido:';
+$_['entry_geo_zone']     = 'Zona Geográfica:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de aparición:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
-$_['error_bank']         = 'Bank Transfer Instructions Required!';
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar los detalles de la transferencia bancaria!';
+$_['error_bank']         = 'Instrucciones de transferencia bancaria obligatorias!';
 ?>
