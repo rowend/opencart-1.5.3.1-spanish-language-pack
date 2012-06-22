0cec49bfdcb371429c37fe8e44c5f9069d9eeb0a
rowend <rowend07@gmail.com>
2012-06-22 11:50:52 -0500

    admin-module-welcome

--- upload/admin/language/spanish/module/welcome.php
+++ upload/admin/language/spanish/module/welcome.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']       = 'Welcome';
+$_['heading_title']       = 'Bienvenido';
 
 // Text
-$_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module welcome!';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
+$_['text_module']         = 'Módulos';
+$_['text_success']        = 'Éxito: has modificado el módulo de bienvenida!';
+$_['text_content_top']    = 'Contenido superior';
+$_['text_content_bottom'] = 'Contenido inferior';
+$_['text_column_left']    = 'Columna izquierda';
+$_['text_column_right']   = 'Columna derecha';
 
 // Entry
-$_['entry_description']   = 'Welcome Message:';
-$_['entry_layout']        = 'Layout:';
-$_['entry_position']      = 'Position:';
-$_['entry_status']        = 'Status:';
-$_['entry_sort_order']    = 'Sort Order:';
+$_['entry_description']   = 'Mensaje de bienvenida:';
+$_['entry_layout']        = 'Diseño:';
+$_['entry_position']      = 'Posición:';
+$_['entry_status']        = 'Estado:';
+$_['entry_sort_order']    = 'Orden de aparición:';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module welcome!';
+$_['error_permission']    = 'Cuidado: No tienes permisos para modificar el módulo de bienvenida!';
 ?>
