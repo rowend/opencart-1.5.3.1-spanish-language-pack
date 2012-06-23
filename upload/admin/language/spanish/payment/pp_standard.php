a3810a7c49aa4c2bbb7a1fe980dc83ad0b3b26a0
rowend <rowend07@gmail.com>
2012-06-23 13:36:11 -0500

    admin-payment-pp_standard

--- upload/admin/language/spanish/payment/pp_standard.php
+++ upload/admin/language/spanish/payment/pp_standard.php
@@ -3,33 +3,33 @@
 $_['heading_title']					 = 'PayPal Standard';
 
 // Text
-$_['text_payment']					 = 'Payment';
-$_['text_success']					 = 'Success: You have modified PayPal account details!';
+$_['text_payment']					 = 'Pago';
+$_['text_success']					 = 'Éxito: has modificado los detalles de la cuenta de Paypal!';
 $_['text_pp_standard']				 = '<a onclick="window.open(\'https://www.paypal.com/uk/mrb/pal=W9TBB5DTD6QJW\');"><img src="view/image/payment/paypal.png" alt="PayPal" title="PayPal" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_authorization']			 = 'Authorization';
-$_['text_sale']						 = 'Sale';
+$_['text_authorization']			 = 'Autorización';
+$_['text_sale']						 = 'Venta';
 
 // Entry
 $_['entry_email']					 = 'E-Mail:';
-$_['entry_test']					 = 'Sandbox Mode:';
-$_['entry_transaction']				 = 'Transaction Method:';
-$_['entry_debug']					 = 'Debug Mode:<br/><span class="help">Logs additional information to the system log.</span>';
-$_['entry_total']                    = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
-$_['entry_canceled_reversal_status'] = 'Canceled Reversal Status:';
-$_['entry_completed_status']         = 'Completed Status:';
-$_['entry_denied_status']			 = 'Denied Status:';
-$_['entry_expired_status']			 = 'Expired Status:';
-$_['entry_failed_status']			 = 'Failed Status:';
-$_['entry_pending_status']			 = 'Pending Status:';
-$_['entry_processed_status']		 = 'Processed Status:';
-$_['entry_refunded_status']			 = 'Refunded Status:';
-$_['entry_reversed_status']			 = 'Reversed Status:';
-$_['entry_voided_status']		     = 'Voided Status:';
-$_['entry_geo_zone']				 = 'Geo Zone:';
-$_['entry_status']					 = 'Status:';
-$_['entry_sort_order']				 = 'Sort Order:';
+$_['entry_test']					 = 'Modo Sandbox (prueba):';
+$_['entry_transaction']				 = 'Método de transacción:';
+$_['entry_debug']					 = 'Modo Debug:<br/><span class="help">Guardar información adicional en los Logs del sistema.</span>';
+$_['entry_total']                    = 'Total:<br /><span class="help">El total de la orden de pago debe llegar antes de que este método de pago sea activado.</span>';
+$_['entry_canceled_reversal_status'] = 'stado de revocación cancelada:';
+$_['entry_completed_status']         = 'Estado completado:';
+$_['entry_denied_status']			 = 'Estado denegado:';
+$_['entry_expired_status']			 = 'Estado caducado:';
+$_['entry_failed_status']			 = 'Estado fallido:';
+$_['entry_pending_status']			 = 'Estado pendiente:';
+$_['entry_processed_status']		 = 'Estado procesado:';
+$_['entry_refunded_status']			 = 'Estado reembolsado:';
+$_['entry_reversed_status']			 = 'Estado reservado:';
+$_['entry_voided_status']		     = 'Estado anulado:';
+$_['entry_geo_zone']				 = 'Zona Geográfica:';
+$_['entry_status']					 = 'Estado:';
+$_['entry_sort_order']				 = 'Orden de aparición:';
 
 // Error
-$_['error_permission']				 = 'Warning: You do not have permission to modify payment PayPal!';
-$_['error_email']					 = 'E-Mail required!';
+$_['error_permission']				 = 'Cuidado: No tienes permisos para modificar PayPal!';
+$_['error_email']					 = 'E-Mail obligatorio!';
 ?>
