534a750deec127484370ba2ef6777602924cd45c
rowend <rowend07@gmail.com>
2012-06-23 12:38:36 -0500

    admin-payment-pp_xpress

--- upload/admin/language/spanish/payment/pp_express.php
+++ upload/admin/language/spanish/payment/pp_express.php
@@ -2,28 +2,28 @@
 // Heading
 $_['heading_title']      = 'PayPal Express';
 
-// Text 
-$_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified PayPal Express Checkout account details!';
+// Text
+$_['text_payment']       = 'Pago';
+$_['text_success']       = 'Éxito: has modificado los  detalles de la cuenta de PayPal Express Checkout!';
 $_['text_pp_express']    = '<a onclick="window.open(\'https://www.paypal.com/uk/mrb/pal=W9TBB5DTD6QJW\');"><img src="view/image/payment/paypal.png" alt="PayPal" title="PayPal" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_authorization'] = 'Authorization';
-$_['text_sale']          = 'Sale';
+$_['text_authorization'] = 'Autorización';
+$_['text_sale']          = 'Venta';
 
 // Entry
-$_['entry_username']     = 'API Username:';
-$_['entry_password']     = 'API Password:';
-$_['entry_signature']    = 'API Signature:';
-$_['entry_test']         = 'Test Mode:';
-$_['entry_method']       = 'Transaction Method:';
-$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
-$_['entry_order_status'] = 'Order Status:';
-$_['entry_geo_zone']     = 'Geo Zone:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_username']     = 'Usuario del API:';
+$_['entry_password']     = 'Contraseña del API:';
+$_['entry_signature']    = 'Firma del API:';
+$_['entry_test']         = 'Modo de Prueba:';
+$_['entry_method']       = 'Método de Transacción:';
+$_['entry_total']        = 'Total:<br /><span class="help">El total de la orden de pago debe llegar antes de que este método de pago sea activado.</span>';
+$_['entry_order_status'] = 'Estado del pedid:';
+$_['entry_geo_zone']     = 'Zona Geográfica:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de aparición:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment PayPal Express Checkout!';
-$_['error_username']     = 'API Username Required!'; 
-$_['error_password']     = 'API Password Required!'; 
-$_['error_signature']    = 'API Signature Required!'; 
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar payment PayPal Express Checkout!';
+$_['error_username']     = 'Usuario del API  obligatorio!';
+$_['error_password']     = 'Contraseña del API  obligatoria!';
+$_['error_signature']    = 'Firma del API  obligatoria!';
 ?>
