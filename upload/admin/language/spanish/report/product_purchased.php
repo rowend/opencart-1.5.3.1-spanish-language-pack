f905d904000b5fff5c33e1e0df4c058688751060
rowend <rowend07@gmail.com>
2012-06-23 19:24:52 -0500

    admin-report-product_purchased

--- upload/admin/language/spanish/report/product_purchased.php
+++ upload/admin/language/spanish/report/product_purchased.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']     = 'Products Purchased Report';
+$_['heading_title']     = 'Reporte de Productos Comprados';
 
 // Text
-$_['text_all_status']   = 'All Statuses';
+$_['text_all_status']   = 'Todos los Estados';
 
 // Column
-$_['column_date_start'] = 'Date Start';
-$_['column_date_end']   = 'Date End';
-$_['column_name']       = 'Product Name';
-$_['column_model']      = 'Model';
-$_['column_quantity']   = 'Quantity';
+$_['column_date_start'] = 'Fecha de Inicio';
+$_['column_date_end']   = 'Fecha de Terminación';
+$_['column_name']       = 'Nombre del Producto';
+$_['column_model']      = 'Modelo';
+$_['column_quantity']   = 'Cantidad';
 $_['column_total']      = 'Total';
 
 // Entry
-$_['entry_date_start']  = 'Date Start:';
-$_['entry_date_end']    = 'Date End:';
-$_['entry_status']      = 'Order Status:';
+$_['entry_date_start']  = 'Fecha de Inicio:';
+$_['entry_date_end']    = 'Fecha de Terminación:';
+$_['entry_status']      = 'Estado del pedido:';
 ?>
