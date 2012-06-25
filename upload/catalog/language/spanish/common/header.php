1b3330dfbdac5ae9310e6b15bb3f50192ee2aefe
DaniGO83 <gonzalezdr@gmail.com>
2012-06-25 13:01:20 -0430

    actualiza el encabezado del catalogo

--- upload/catalog/language/spanish/common/header.php
+++ upload/catalog/language/spanish/common/header.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_home']           = 'Home';
-$_['text_wishlist']       = 'Wish List (%s)';
-$_['text_shopping_cart']  = 'Shopping Cart';
-$_['text_search']         = 'Search';
-$_['text_welcome']        = 'Welcome visitor you can <a href="%s">login</a> or <a href="%s">create an account</a>.';
-$_['text_logged']         = 'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
-$_['text_account']        = 'My Account';
-$_['text_checkout']       = 'Checkout';
+$_['text_home']           = 'Inicio';
+$_['text_wishlist']       = 'Lista de deseos (%s)';
+$_['text_shopping_cart']  = 'Carrito de Compras';
+$_['text_search']         = 'Buscar';
+$_['text_welcome']        = '¡Bienvenido, usted puede hacer <a href="%s">Iniciar sesion</a> o <a href="%s">crear una nueva cuenta</a>.';
+$_['text_logged']         = 'Usted inicio sesion como <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
+$_['text_account']        = 'Mi Cuenta';
+$_['text_checkout']       = 'Proceder a Pagar';
 ?>
