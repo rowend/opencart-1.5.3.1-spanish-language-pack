66d31fdf0a170ea2b6698e82ceb994aea5e20458
DaniGO83 <gonzalezdr@gmail.com>
2012-06-25 13:15:09 -0430

    actualiza el encabezado del catalogo

--- upload/catalog/language/spanish/common/header.php
+++ upload/catalog/language/spanish/common/header.php
@@ -4,8 +4,8 @@ $_['text_home']           = 'Inicio';
 $_['text_wishlist']       = 'Lista de deseos (%s)';
 $_['text_shopping_cart']  = 'Carrito de Compras';
 $_['text_search']         = 'Buscar';
-$_['text_welcome']        = '¡Bienvenido, usted puede hacer <a href="%s">Iniciar sesion</a> o <a href="%s">crear una nueva cuenta</a>.';
-$_['text_logged']         = 'Usted inicio sesion como <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
+$_['text_welcome']        = '¡Bienvenido, usted puede <a href="%s">Iniciar sesion</a> o <a href="%s">crear una nueva cuenta</a>.';
+$_['text_logged']         = 'Usted inicio sesion como <a href="%s">%s</a> <b>(</b> <a href="%s">Cerrar Sesion</a> <b>)</b>';
 $_['text_account']        = 'Mi Cuenta';
 $_['text_checkout']       = 'Proceder a Pagar';
 ?>
