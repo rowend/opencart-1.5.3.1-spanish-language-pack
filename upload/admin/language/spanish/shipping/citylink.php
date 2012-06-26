11bc9694e4770b179220cb06cf0a5f867518c1a6
rowend <rowend07@gmail.com>
2012-06-26 11:20:12 -0500

    admin-shipping-flat

--- upload/admin/language/spanish/shipping/citylink.php
+++ upload/admin/language/spanish/shipping/citylink.php
@@ -9,7 +9,7 @@ $_['text_success']     = 'Éxito: has modificado el envío Citylink!';
 // Entry
 $_['entry_rate']       = 'Tarifas de Citylink:<br /><span class="help">Introduce valores hasta 5,2 decimales. 12345.67) Ejemplo: .1:1,.25:1.27 - Pesos menores o iguales que 0.1Kg costarían &pound;1.00, Pesos menores o iguales que 0.25kg pero más que 0.1Kg costarían 1.27. No introducir KG o símbolos.</span>';
 $_['entry_tax_class']  = 'Tipo de impuesto:';
-$_['entry_geo_zone']   = 'Geo Zone:';
+$_['entry_geo_zone']   = 'Zona Geográfica:';
 $_['entry_status']     = 'Estado:';
 $_['entry_sort_order'] = 'Orden de aparición:';
 
