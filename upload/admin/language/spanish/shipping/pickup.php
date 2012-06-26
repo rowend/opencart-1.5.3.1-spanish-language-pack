ed1b23a3eb43ed19e730b4af12b6dbd874b4746c
rowend <rowend07@gmail.com>
2012-06-26 12:00:25 -0500

    admin-shipping-pickup

--- upload/admin/language/spanish/shipping/pickup.php
+++ upload/admin/language/spanish/shipping/pickup.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']    = 'Pickup From Store';
+$_['heading_title']    = 'Recogida en tienda';
 
 // Text 
-$_['text_shipping']    = 'Shipping';
-$_['text_success']     = 'Success: You have modified pickup from store!';
+$_['text_shipping']    = 'Envío';
+$_['text_success']     = 'Éxito: has modificado el apartado recogida en tienda!';
 
 // Entry
-$_['entry_geo_zone']   = 'Geo Zone:';
-$_['entry_status']     = 'Status:';
-$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_geo_zone']   = 'Zona Geográfica:';
+$_['entry_status']     = 'Estado:';
+$_['entry_sort_order'] = 'Orden de aparición:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify pickup from store!';
+$_['error_permission'] = 'Cuidado: No tienes permisos para modificar el apartado recogida en tienda!';
 ?>
