63c96da2942db6a00f668de5a3b6630034b5ad90
rowend <rowend07@gmail.com>
2012-06-27 01:27:15 -0500

    admin-user-user

--- upload/admin/language/spanish/user/user.php
+++ upload/admin/language/spanish/user/user.php
@@ -1,35 +1,35 @@
 <?php
 // Heading
-$_['heading_title']     = 'User';
+$_['heading_title']     = 'Usuario';
 
 // Text
-$_['text_success']      = 'Success: You have modified users!';
+$_['text_success']      = 'Éxito: Has modificado los usuarios!';
 
 // Column
-$_['column_username']   = 'Username';
-$_['column_status']     = 'Status';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_username']   = 'Nombre de Usuario';
+$_['column_status']     = 'Estado';
+$_['column_date_added'] = 'Fecha de alta';
+$_['column_action']     = 'Acción';
 
 // Entry
-$_['entry_username']   = 'Username:';
-$_['entry_password']   = 'Password:';
-$_['entry_confirm']    = 'Confirm:';
-$_['entry_firstname']  = 'First Name:';
-$_['entry_lastname']   = 'Last Name:';
+$_['entry_username']   = 'Nombre de Usuario:';
+$_['entry_password']   = 'Contraseña:';
+$_['entry_confirm']    = 'Confirmar:';;
+$_['entry_firstname']  = 'Nombre:';
+$_['entry_lastname']   = 'Apellido/s:';
 $_['entry_email']      = 'E-Mail:';
-$_['entry_user_group'] = 'User Group:';
-$_['entry_status']     = 'Status:';
-$_['entry_captcha']    = 'Enter the code in the box below:';
+$_['entry_user_group'] = 'Grupo de usuarios:';
+$_['entry_status']     = 'Estado:';
+$_['entry_captcha']    = 'Introduce el código en la casilla inferior:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify users!';
-$_['error_account']    = 'Warning: You can not delete your own account!';
-$_['error_exists']     = 'Warning: Username is already in use!';
-$_['error_username']   = 'Username must be between 3 and 20 characters!';
-$_['error_password']   = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']    = 'Password and password confirmation do not match!';
-$_['error_firstname']  = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']   = 'Last Name must be between 1 and 32 characters!';
-$_['error_captcha']    = 'Verification code does not match the image!';
+$_['error_permission'] = 'Cuidado: No tienes permiso para modificar los usuarios!!';
+$_['error_account']    = 'Cuidado: No puedes borrar tu propia cuenta!';
+$_['error_exists']     = 'Cuidado: Este Nombre de Usuario ya está en Uso!';
+$_['error_username']   = 'El nombre de usuario debe tener entre 3 y 20 carácteres!';
+$_['error_password']   = 'La contraseña debe tener entre 4 y 20 carácteres!!';
+$_['error_confirm']    = 'La contraseña y la confirmación de la contraseña no coinciden!';
+$_['error_firstname']  = 'El nombre debe tener entre 1 y 32 carácteres!';
+$_['error_lastname']   = 'El apellido debe tener entre 1 y 32 carácteres!';
+$_['error_captcha']    = 'El código de verificación no coincide con la imagen!';
 ?>
