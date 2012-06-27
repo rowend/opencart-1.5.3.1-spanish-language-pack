c19f1de57178a2e155ad8d52c575826b97daddf5
rowend <rowend07@gmail.com>
2012-06-27 11:52:03 -0500

    catalog-payment-klarna

--- upload/catalog/language/spanish/payment/klarna_invoice.php
+++ upload/catalog/language/spanish/payment/klarna_invoice.php
@@ -1,23 +1,23 @@
 <?php
 // Text
-$_['text_title']       = 'Klarna Invoice';
-$_['text_information'] = 'Klarna Information';
-$_['text_additional']  = 'Klarna requires some additional information before they can proccess your order.';
-$_['text_wait']        = 'Please wait!';
-$_['text_male']        = 'Male';
-$_['text_female']      = 'Female';
+$_['text_title']       = 'Factura de Klarna';
+$_['text_information'] = 'Información de Klarna';
+$_['text_additional']  = 'Klarna requiere alguna información adiccional antes de que ellos puedan procescar tu pedido.';
+$_['text_wait']        = 'Por favor espere!';
+$_['text_male']        = 'Hombre';
+$_['text_female']      = 'Mujer';
 
 // Entry
-$_['entry_gender']     = 'Gender:';
-$_['entry_pno']        = 'PNO / BIRTH DATA:<span class="help">(07071960)</span>';
-$_['entry_house_no']   = 'House No.:';
-$_['entry_house_ext']  = 'House Ext.:';
-$_['entry_cellno']     = 'Cell Phone:';
+$_['entry_gender']     = 'Genero:';
+$_['entry_pno']        = 'PNO / FECHA DE NACIMIENTO:<span class="help">(07071960)</span>';
+$_['entry_house_no']   = 'Número de la Casa:';
+$_['entry_house_ext']  = 'Extensión de la Casa:';
+$_['entry_cellno']     = 'Teléfono Celular:';
 
 // Error
-$_['error_gender']     = 'Gender Required!';
-$_['error_dob']        = 'Date of birth Required!';
-$_['error_house_no']   = 'House No. Required!';
-$_['error_house_ext']  = 'House Extension Required!';
-$_['error_dob']        = 'Date of birth Required!';
+$_['error_gender']     = 'Genero obligatorio!';
+$_['error_dob']        = 'Fecha de nacimiento obligatoria!';
+$_['error_house_no']   = 'Número de la casa obligatorio!';
+$_['error_house_ext']  = 'Extensión de la Casa obligatorio!';
+$_['error_dob']        = 'Fecha de nacimiento obligatoria!';
 ?>
