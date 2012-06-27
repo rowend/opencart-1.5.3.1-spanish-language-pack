398be083300501aee81905a5abbfb48b99358fb5
rowend <rowend07@gmail.com>
2012-06-27 01:09:05 -0500

    admin-total-tax

--- upload/admin/language/spanish/total/tax.php
+++ upload/admin/language/spanish/total/tax.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Taxes';
+$_['heading_title']    = 'Impuestos';
 
 // Text
-$_['text_total']       = 'Order Totals';
-$_['text_success']     = 'Success: You have modified taxes total!';
+$_['text_total']       = 'Totales del pedido';
+$_['text_success']     = 'Éxito: has modificado el total de impuestos!';
 
 // Entry
-$_['entry_status']     = 'Status:';
-$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_status']     = 'Estado:';
+$_['entry_sort_order'] = 'Orden de aparición:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify taxes total!';
+$_['error_permission'] = 'Cuidado: No tienes permisos para modificar el total de impuestos!';
 ?>
