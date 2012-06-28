d288ca65c474d249198ba1956e722df0bffaefe4
rowend <rowend07@gmail.com>
2012-06-28 12:51:15 -0500

    catalog-account-reward

--- upload/catalog/language/spanish/account/reward.php
+++ upload/catalog/language/spanish/account/reward.php
@@ -1,15 +1,15 @@
 <?php
 // Heading 
-$_['heading_title']      = 'Your Reward Points';
+$_['heading_title']      = 'Tus puntos de recompensa';
 
 // Column
-$_['column_date_added']  = 'Date Added';
-$_['column_description'] = 'Description';
-$_['column_points']      = 'Points';
+$_['column_date_added']  = 'Fecha de alta';
+$_['column_description'] = 'Descripción';
+$_['column_points']      = 'Puntos';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_reward']        = 'Reward Points';
-$_['text_total']         = 'Your total number of reward points is:';
-$_['text_empty']         = 'You do not have any reward points!';
+$_['text_account']       = 'Cuenta';
+$_['text_reward']        = 'Puntos de recompensa';
+$_['text_total']         = 'El total de tus puntos de recompensa es:';
+$_['text_empty']         = 'o tienes ningún punto de recompensan!';
 ?>
