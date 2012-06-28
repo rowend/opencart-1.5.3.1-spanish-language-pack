4b4cda74b0bd9df900f2cddba2e5bd4406b9c9b8
rowend <rowend07@gmail.com>
2012-06-28 12:02:59 -0500

    catalog-account-logout

--- upload/catalog/language/spanish/account/logout.php
+++ upload/catalog/language/spanish/account/logout.php
@@ -1,9 +1,9 @@
 <?php
 // Heading 
-$_['heading_title'] = 'Account Logout';
+$_['heading_title'] = 'Salir';
 
 // Text
-$_['text_message']  = '<p>You have been logged off your account. It is now safe to leave the computer.</p><p>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_message']  = '<p>Has cerrado sesión. Ahora puedes abandonar el ordenador.</p><p>Tu carrito  ha sido guardado, los items en la cesta se restaurarán cuando inicies sesión de nuevo con tu cuenta.</p>';
+$_['text_account']  = 'Cuenta';
+$_['text_logout']   = 'Salir';
 ?>
