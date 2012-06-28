8d0d8a6c8d3ffe19eac9d3a594d5ec8820e8200b
rowend <rowend07@gmail.com>
2012-06-28 10:58:29 -0500

    somehting

--- upload/catalog/language/spanish/account/account.php
+++ upload/catalog/language/spanish/account/account.php
@@ -5,16 +5,16 @@ $_['heading_title']      = 'Mi cuenta';
 // Text
 $_['text_account']       = 'Cuenta';
 $_['text_my_account']    = 'Mi cuenta';
-$_['text_my_orders']     = 'Mis pedidos';
-$_['text_my_newsletter'] = 'Boletín de noticias';
-$_['text_edit']          = 'Editar tu información de cuenta';
-$_['text_password']      = 'Cambia tu contraseña';
-$_['text_address']       = 'Modificar tu libreta de direcciones';
+$_['text_my_orders']     = 'Mis ordenes';
+$_['text_my_newsletter'] = 'Boletin de noticias';
+$_['text_edit']          = 'Editar mi cuenta';
+$_['text_password']      = 'Cambiar contraseña';
+$_['text_address']       = 'Modificar libreta de direcciones';
 $_['text_wishlist']      = 'Modificar mi lista de deseos';
 $_['text_order']         = 'Ver mi historial de compras';
 $_['text_download']      = 'Descargas';
-$_['text_reward']        = 'Tus puntos de recompensa'; 
-$_['text_return']        = 'Ver tus peticiones de devolución'; 
-$_['text_transaction']   = 'Tus transacciones'; 
-$_['text_newsletter']    = 'Suscribirse / Darse de baja al Boletín de Noticias';
+$_['text_reward']        = 'Mis puntos de recompensa'; 
+$_['text_return']        = 'Ver mis solicitudes de devolucion'; 
+$_['text_transaction']   = 'Mis transacciones'; 
+$_['text_newsletter']    = 'Suscribirse / Eliminar subscripcion al Boletin de Noticias';
 ?>
