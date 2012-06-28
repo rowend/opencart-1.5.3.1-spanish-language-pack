bcfbd6ddcc389916187f6981dc100c85df7894a6
rowend <rowend07@gmail.com>
2012-06-28 11:07:41 -0500

    catalog-account-address

--- upload/catalog/language/spanish/account/address.php
+++ upload/catalog/language/spanish/account/address.php
@@ -4,35 +4,35 @@ $_['heading_title']     = 'Libreta de direcciones';
 
 // Text
 $_['text_account']      = 'Cuenta';
-$_['text_address_book'] = 'Entrada de libreta de direcciones';
-$_['text_edit_address'] = 'Editar direccion';
-$_['text_insert']       = 'Su direccion ha sido agregada satisfactoriamente';
-$_['text_update']       = 'Su direccion ha sido actualizada satisfactoriamente';
-$_['text_delete']       = 'Su direccion ha sido eliminada satisfactoriamente';
+$_['text_address_book'] = 'Entradas de la libreta de direcciones';
+$_['text_edit_address'] = 'Editar dirección';
+$_['text_insert']       = 'Su dirección ha sido agregada satisfactoriamente';
+$_['text_update']       = 'Su dirección ha sido actualizada satisfactoriamente';
+$_['text_delete']       = 'Su dirección ha sido eliminada satisfactoriamente';
 
 // Entry
 $_['entry_firstname']   = 'Nombre:';
-$_['entry_lastname']    = 'Apellido:';
-$_['entry_company']     = 'Empresa:';
-$_['entry_company_id']  = 'N° de id de Empresa:';
-$_['entry_tax_id']      = 'N° de identificacion tributario:';
-$_['entry_address_1']   = 'Direccion 1:';
-$_['entry_address_2']   = 'Direccion 2:';
-$_['entry_postcode']    = 'Codigo Postal:';
+$_['entry_lastname']    = 'Apellido/s:';
+$_['entry_company']     = 'Compañía:';
+$_['entry_company_id']  = 'Id de la Empresa:';
+$_['entry_tax_id']      = 'Id del impuesto:';
+$_['entry_address_1']   = 'Dirección 1:';
+$_['entry_address_2']   = 'Dirección 2:';
+$_['entry_postcode']    = 'Código Postal:';
 $_['entry_city']        = 'Ciudad:';
 $_['entry_country']     = 'Pais:';
-$_['entry_zone']        = 'Region o Estado:';
-$_['entry_default']     = 'Direccion predeterminada:';
+$_['entry_zone']        = 'Región / Estado:';
+$_['entry_default']     = 'Dirección por defecto:';
 
 // Error
-$_['error_delete']      = '¡Advertencia! Usted debe agregar al menos una direccion';
-$_['error_default']     = '¡Advertencia! Usted no puede eliminar su direccion predeterminada!';
-$_['error_firstname']   = 'El campo "Nombre" debe tener entre 1 y 32 caracteres';
-$_['error_lastname']    = 'El campo "Apellido" debe tener entre 1 y 32 caracteres';
-$_['error_vat']         = 'El codigo de identificacion fiscal es invalido';
-$_['error_address_1']   = 'El campo "Direccion" debe tener entre 3 y 128 caracteres';
-$_['error_postcode']    = 'El campo Codigo Postal debe tener entre 2 y 10 caracteres';
-$_['error_city']        = 'El campo ciudad deber tener entre 2 y 128 caracteres';
-$_['error_country']     = 'Por favor seleccione un pais';
-$_['error_zone']        = 'Por favor seleccione una region o estado';
+$_['error_delete']      = 'Cuidado: Usted debe agregar al menos una dirección';
+$_['error_default']     = 'Cuidado: Usted no puede eliminar su dirección por defecto!';
+$_['error_firstname']   = 'El campo Nombre debe tener entre 1 y 32 carácteres';
+$_['error_lastname']    = 'El campo Apellido debe tener entre 1 y 32 carácteres';
+$_['error_vat']         = 'El valor del impuesto es invalido';
+$_['error_address_1']   = 'El campo Dirección debe tener entre 3 y 128 carácteres';
+$_['error_postcode']    = 'El campo Código Postal debe tener entre 2 y 10 carácteres';
+$_['error_city']        = 'El campo ciudad deber tener entre 2 y 128 carácteres';
+$_['error_country']     = 'Por favor seleccione un país';
+$_['error_zone']        = 'Por favor seleccione una región o estado';
 ?>
